60f52928c9aa4bf760f88e7864b2eac27f0f4647
Lev Yuvensky <levayu22@gmail.com>
2022-12-14 16:30:22 +0300

    Add empty main page

--- shop/app/views.py
+++ shop/app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.shortcuts import render
+
+
+def main_page(request):
+    return render(request, 'app/main_page.html', {})

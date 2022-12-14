db84b279757292cfe4daedf3b622422a18ed7350
Maks <mloboda@edu.hse.ru>
2022-12-14 17:02:48 +0300

    Added extra fields for order count

--- sql/order_count_per_user.sql
+++ sql/order_count_per_user.sql
@@ -1,1 +1,7 @@
-SELECT "Код клиента", count(distinct "Код заказа") as order_count from "Заказ клиента" group by "Код клиента"
+SELECT users.*, orders.order_count
+    from
+    (
+        SELECT "Код клиента", count(distinct "Код заказа") as order_count
+        from "Заказ клиента" group by "Код клиента"
+    ) as orders
+    join Клиент as users on orders."Код клиента" == users."Код клиента"

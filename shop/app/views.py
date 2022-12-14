6542c36d9b9f4cedb981ce5ed75e076b596a807e
Seq <peogne@gmail.com>
2022-12-14 18:21:58 +0400

    Add route for get count customer

--- shop/app/views.py
+++ shop/app/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+import json
 
-
 from django.shortcuts import render
+from django.db import connection
+from django.http import HttpResponse
 
 
 def main_page(request):
     return render(request, 'app/main_page.html', {})
+
+
+def get_count_client(request):
+    cursor = connection.cursor()
+    cursor.execute('''SELECT count(distinct "Код клиента") from Клиент''')
+    result = cursor.fetchone()
+    response_data = {
+        'data': result[0],
+        'status': 200,
+        'message': 'Ok.'
+    }
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+
